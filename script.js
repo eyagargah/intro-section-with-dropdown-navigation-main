a298a7edd9b7d8a4b334b11839fe22111515def3
Eya Gargah <eyagargah@hotmail.com>
2023-04-07 23:59:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ let dropDownContent = document.querySelectorAll('.dropdown-content')
 
 dropBtn.forEach(function(item){
   item.addEventListener('click', function(){
-    item.parentElement.lastElementChild.style.display ="block"
+    if ( item.parentElement.lastElementChild.classList.contains('hide')){
+      //show dropdown menu
+      item.parentElement.lastElementChild.classList.remove('hide')
+      item.parentElement.lastElementChild.classList.add('show')
+      //toggle arrows
+      item.childNodes[1].classList.remove('hide')
+      item.childNodes[1].classList.add('show')
+
+      item.childNodes[3].classList.remove('show')
+      item.childNodes[3].classList.add('hide')
+
+    }else {
+      item.parentElement.lastElementChild.classList.remove('show')
+      item.parentElement.lastElementChild.classList.add('hide')
+
+      item.childNodes[1].classList.remove('show')
+      item.childNodes[1].classList.add('hide')
+
+      item.childNodes[3].classList.remove('hide')
+      item.childNodes[3].classList.add('show')
+    }
   })
 })
